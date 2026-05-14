8365aeaa4a0df5ca65d1b32f4aabb51b48f762f6
prakash <prakashsaha1999@gmail.com>
2026-05-14 17:54:29 +0530

    fix: allow both www and non-www origins in CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,12 +8,14 @@ export default ({ env }: { env: any }) => [
     name: 'strapi::cors',
     config: {
       origin: [
-        env('FRONTEND_URL', 'http://localhost:3000'), 
+        env('FRONTEND_URL', 'https://krafttreasure.com'), 
+        'https://krafttreasure.com', 
+        'https://www.krafttreasure.com',
         'http://localhost:3000', 
         'http://127.0.0.1:3000'
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
       keepHeaderOnError: true,
     },
   },
