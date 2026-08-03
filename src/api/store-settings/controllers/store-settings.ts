e7dfd98a5e1424884aac5bf926aea0bfb408ecd1
Prakash Saha <prakashsaha1999@gmail.com>
2026-08-03 16:48:04 +0530

    fix(cms): bypass ts type check for new store-settings api during build

--- src/api/store-settings/controllers/store-settings.ts
+++ src/api/store-settings/controllers/store-settings.ts
@@ -4,4 +4,5 @@
 
 import { factories } from '@strapi/strapi';
 
+// @ts-ignore
 export default factories.createCoreController('api::store-settings.store-settings');
--- src/api/store-settings/routes/store-settings.ts
+++ src/api/store-settings/routes/store-settings.ts
@@ -4,6 +4,7 @@
 
 import { factories } from '@strapi/strapi';
 
+// @ts-ignore
 export default factories.createCoreRouter('api::store-settings.store-settings', {
   config: { find: { auth: false }, findOne: { auth: false } },
 });
